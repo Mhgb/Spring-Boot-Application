1aebea10ba295d2a689f52b68c43a4a701f3ceb0
Ganesh Babu M H <mhgb.dev@gmail.com>
2022-12-04 01:18:03 +0530

    property file added to setup H2 database

--- src/main/java/com/mhgb/trainsystem/TrainSystemApplication.java
+++ src/main/java/com/mhgb/trainsystem/TrainSystemApplication.java
@@ -1,4 +1,4 @@
-package com.mhgb.trainsystem.application;
+package com.mhgb.trainsystem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
